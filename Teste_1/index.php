f75cb779b504139280fd7256f2c943b24d310a1a
JVSATSU <84157543+JVSATSU@users.noreply.github.com>
2022-01-25 16:52:45 +0000

    Update index.php

--- Teste_1/index.php
+++ Teste_1/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,47 +13,53 @@
     </style>
     <title>Teste 1</title>
 </head>
+
 <body>
-<?php
-    $array = array(6,9,1,2,8,"A",7,4,10,3,5,12,"E","C",75,99,"ABCDEFG",1.5,100);
-	ordena($array);
-	
-	function ordena($numeros){
-        $nulo = null;
-        for($i = 0; $i < count($numeros); $i++){  
-     
-            
-        
-            if($i < count($numeros) - 1)
+    <?php    
+    
+    
+    $array  = array(6, 9, 1, 2, 8, "A", 7, 4, 10, 3, 5, 12, "E", "C", 75, 99, "ABCDEFG", 1.5, 100,111);
+    ordenaNumeros($array);
+    
+    function ordenaNumeros (array $num_array)
+    {
+        $nulo = NULL;
+        for ($i = 0; $i < count($num_array); $i++) 
+        {
+
+
+
+            if ($i < count($num_array) - 1) 
+            {
                 $prox = $i + 1;
-            
-            
-                for($ant = 0; $ant < count($numeros); $ant++){
-                if($numeros[$ant] > $numeros[$prox]){
-                    $nulo = $numeros[$ant];
-                    $numeros[$ant] = $numeros[$prox];
-                    $numeros[$prox] = $nulo;
+
+
+                for ($ant = 0; $ant < count($num_array); $ant++) 
+                {
+                    if ($num_array[$ant] > $num_array[$prox]) 
+                    {
+                        $nulo = $num_array[$ant];
+                        $num_array[$ant] = $num_array[$prox];
+                        $num_array[$prox] = $nulo;
+                    }
                 }
             }
-            if(!is_int($numeros[$i])){
-                $numeros[$i] = NULL;
-            } 
         }
 
-        
-        for($i = 0; $i < count($numeros); $i++){
-            
-            if(!is_int($numeros[$i])){
-                $numeros[$i] = NULL;
-            } 
-            echo $numeros[$i], ' ';
+
+        for ($i = 0; $i < count($num_array); $i++) 
+        {
+
+            if (!is_int($num_array[$i])) 
+            {
+                $num_array[$i] = NULL;
+            }
+
+            echo $num_array[$i], ' ';
         }
     }
 
-    
-    
-    
-    
-?>
+    ?>
 </body>
+
 </html>
