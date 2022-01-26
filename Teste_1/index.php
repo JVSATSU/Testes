e93cbc4761b2ac85c8405158a242bbf1f102ee9a
JVSATSU <84157543+JVSATSU@users.noreply.github.com>
2022-01-26 00:49:14 +0000

    Update index.php

--- Teste_1/index.php
+++ Teste_1/index.php
@@ -12,16 +12,16 @@
             if ($i < count((array)$num_array) - 1) //compara o numero atual e anterior até o ultimo indice do array
             {
                 $proximo = $i + 1; //proximo = indice atual + 1
-                $troca = NULL; //esta variavel é utilizada para fazer a mundança dos indices do array (aceito sugestões).
+                $troca = NULL; //esta variavel é utilizada para fazer a mundança dos indices do array (aceito sugestões de nome pra variavel).
                 for ((int)$atual = 0; (int)$atual < count((array)$num_array); (int)$atual++)// enquanto o indice atual for menor que o tamanho do array,
                 {                                                                           // irá ser verificado se o indice atual é maior que o proximo indice.
                     if ($num_array[$atual] > $num_array[$proximo]) 
                     {
-                        $troca = $num_array[$atual];                 //variavel troca recebe o indice atual             -------> 2(troca) -----
+                        $troca = $num_array[$atual];                 //variavel troca recebe o indice atual              -------> 2(troca) ----
                         $num_array[$atual] = $num_array[$proximo];   //o indice atual se torna o proximo                |                      |
                         $num_array[$proximo] = $troca;               //e o proximo agora é o indice atual           2(atual)              1(proxima)
-                    }                                                //                                                 |                       |
-                }                                                   //                                                  ----------- 1 <---------
+                    }                                                //                                                 |                      |
+                }                                                    //                                                  ----------- 1 <-------
             }
         }
 
